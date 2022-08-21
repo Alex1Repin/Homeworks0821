f8411a356d220502278139c830f5121a7295b412
Alex <al.repin.a@yandex.ru>
2022-08-22 00:02:49 +0300

    add Task_8

---
+++ Task_8/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите число N: ");
+int numberN = int.Parse(Console.ReadLine());
+
+int num = 2;
+
+if (num > numberN)
+{
+    Console.WriteLine("Здесь нет четных чисел");
+}
+
+while (num <= numberN)
+{
+    Console.WriteLine(num);
+    num +=2;
+}
+
+
+
+
+    
