4d8910bc3e4ba76f39719bf622fef22320dbf8c7
Alex <al.repin.a@yandex.ru>
2022-08-21 20:22:45 +0300

    add Task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите число A: ");
+int numberA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число B: ");
+int numberB = int.Parse(Console.ReadLine());
+
+int max = numberA;
+int min = numberB;
+
+if (numberB > max)
+{
+    max = numberB;
+    min = numberA;
+}
+
+else
+{
+    min = numberB;
+}
+
+Console.Write("max = ");
+Console.WriteLine(max);
+Console.Write("min = ");
+Console.WriteLine(min);
+  
