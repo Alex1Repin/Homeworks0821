ab4bbba8469633f9f3244bb14f362f1b089491fb
Alex <al.repin.a@yandex.ru>
2022-08-21 22:34:39 +0300

    add Task_6

---
+++ Task_6/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число : ");
+int number = int.Parse(Console.ReadLine());
+
+int rem = number % 2;
+
+if (rem == 0)
+{
+    Console.WriteLine("Число четное");
+}
+
+else
+{
+    Console.WriteLine("Число не четное");
+}    
